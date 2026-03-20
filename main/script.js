c9f30dc543bd3b6f0b461998ed2d0c028cf30f72
Dilshad <dilshad08kanpur@gmail.com>
2026-03-20 18:43:04 +0530

    added college portal

---
+++ main/script.js
@@ -0,0 +1,18 @@
+// Redirect pages
+
+document.getElementById("student").onclick = function() {
+    window.location.href = "student.html";
+};
+
+document.getElementById("professional").onclick = function() {
+    window.location.href = "professional.html";
+};
+
+document.getElementById("parents").onclick = function() {
+    window.location.href = "parents.html";
+};
+
+// Login button
+document.querySelector(".login-btn").onclick = function() {
+    alert("Login page coming soon");
+};
